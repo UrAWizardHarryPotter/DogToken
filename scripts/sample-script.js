1afd74985ed3318d9507bcba782521edf1dbaa2e
Daniel Nolan <DanNolan99@gmail.com>
2021-06-01 13:40:13 -0400

    init

---
+++ scripts/sample-script.js
@@ -0,0 +1,15 @@
+async function main() {
+  const Token = await hre.ethers.getContractFactory("DOGToken");
+  const initialSupply = hre.ethers.utils.parseEther("1000");
+  const token = await Token.deploy(initialSupply);
+  await token.deployed();
+
+  console.log("Token deployed to:", token.address);
+}
+
+main()
+  .then(() => process.exit(0))
+  .catch(error => {
+    console.error(error);
+    process.exit(1);
+  });
